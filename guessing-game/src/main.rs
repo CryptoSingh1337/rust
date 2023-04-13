a7c1b1718f3596dea8c04f61ac1fcc4c37de4b47
Saransh Kumar <cryptosingh@gmail.com>
2023-04-13 10:03:32 +0000

    Fix guessing game

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -4,11 +4,10 @@ use std::io;
 
 fn main() {
     println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..=100); // generating a random number in the range [1, 100]
+    println!("The secret number is: {}", secret_number);
     loop {
         println!("Please input your guess.");
-
-        let secret_number = rand::thread_rng().gen_range(1..=100); // generating a random number in the range [1, 100]
-        println!("The secret number is: {}", secret_number);
 
         let mut guess = String::new();
         io::stdin()
